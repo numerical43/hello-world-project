3c671a3950833571da321d3c63d0e63cce7b979f
numerical43 <numerical43@daum.net>
2023-05-31 16:12:42 +0900

    Hell World! 추가

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,5 +3,6 @@ package com.example;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hell World!!");
     }
 }
